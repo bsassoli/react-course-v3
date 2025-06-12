757060bf96d9bf258eb1ce74d48233d69d0a09ed
Bernardino Sassoli <sassoli@gmail.com>
2025-06-12 21:17:09 +0200

    modify app

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,8 +1,13 @@
+import Starter from "./tutorial/02-useEffect/starter/04-fetch-data";
+import Final from "./tutorial/02-useEffect/final/04-fetch-data";
 function App() {
   return (
-    <div className='container'>
-      <h2>Advanced React</h2>
-    </div>
+    <>
+      <h1>Starter</h1>
+      <Starter />
+      <h1>Final</h1>
+      <Final />
+    </>
   );
 }
 
--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,32 @@
 const url = 'https://api.github.com/users';
-
+import { useState, useEffect } from 'react';
+const User = (user) => {
+  return <div className='user'>
+    <img src={user.avatar_url} alt={user.login} />
+    <h4>{user.login}</h4>
+  </div>
+};
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const retrieveData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+    }
+    catch (error) {
+      console.log(error);
+    };
+};
+
+const [users, setUsers] = useState(retrieveData());
+const renderedUsers = users.map((user) => {
+  return <User key={user.id} {...user} />;
+}
+);
+
+return  <div className='users'>
+  <h2>github users</h2>
+  {renderedUsers} 
+</div>
 };
 export default FetchData;
